d9b387aed779a5738d55ead3914cef75dd00ba43
ehendrix23 <hendrix_erik@hotmail.com>
2021-08-09 13:55:09 -0600

    Light switch (#52)
    * Bump pymyq to 3.0.4 (#19)
    
    * Bump hacs/action from 21.2.0 to 21.2.2 (#15)
    
    Bumps [hacs/action](https://github.com/hacs/action) from 21.2.0 to 21.2.2.
    - [Release notes](https://github.com/hacs/action/releases)
    - [Commits](https://github.com/hacs/action/compare/21.2.0...669a92901c858c7724eca7cf8e3de59d376b10e7)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump pymyq to 3.0.4 to fix authentication error
    
    * Update what is fixed
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Update manifest.json for new pymyq version
    
    * Bump to pymyq 3.1.0b5
    
    * Handle if close/open returns bool instead of task.
    
    * Bump to v1.0.0b7
    
    * Added lights and change history
    
    * Updated to pymyq 3.1.0 and synched with HASS
    
    * Synched with HASS
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- custom_components/myq/const.py
+++ custom_components/myq/const.py
@@ -32,7 +32,7 @@ MYQ_GATEWAY = "myq_gateway"
 MYQ_COORDINATOR = "coordinator"
 
 # myq has some ratelimits in place
-# and 15 seemed to be work every time
+# and 61 seemed to be work every time
 UPDATE_INTERVAL = 15
 
 # Estimated time it takes myq to start transition from one
